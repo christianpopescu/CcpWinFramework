0baa4771e0d3ce838c8d91a067bad59f98d24728
Christian Popescu <christian.popescu@outlook.com>
2019-09-19 00:24:01 +0200

    Init with command line runner

---
+++ src/CCPWinFramework/ConsoleSandbox/ProcessesAndThreads/ProcessesAndThreads.cs
@@ -0,0 +1,33 @@
+﻿
+
+using ProcessesAndThreads.CmdLine;
+
+namespace ConsoleSandbox.ProcessesAndThreads
+{
+    public static class ProcessesAndThreads
+    {
+        /// <summary>
+        /// Executes dir command on some folder
+        /// </summary>
+        public static void RunSomeApplication()
+        {
+            var cmdLine = new CmdLineRunner();
+            cmdLine.RunSyncUtil(@"svn", @"");
+        }
+
+        public static string RunSomeApplicationWithConsole()
+        {
+            var cmdLine = new CmdLineRunner();
+            // return cmdLine.RunSyncUtilGetOutput(@"svn",@"log --xml", @"E:\ccp_vhdd_main\Professional\App");
+            return cmdLine.RunSyncUtilGetOutput(@"D:\Program Files\7-Zip\7z.exe", @"a E:\Temp\ToDelete\archives\archives.zip E:\Temp\ConsoleApplication4\ "
+                  , @"E:\Temp\ToDelete\archives");
+
+        }
+
+        //public static string RunArchiverTool()
+        //{
+
+        //}
+    }
+
+}
---
+++ src/CCPWinFramework/ConsoleSandbox/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Mail;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSandbox
+{
+    /// <summary>
+    /// Console Application used to test different composants
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var output = ProcessesAndThreads.ProcessesAndThreads.RunSomeApplicationWithConsole();
+            Console.Write(output);
+            //TestSendMailMessage();
+            Console.Write("Press Any Key! ");
+            Console.ReadKey();
+        }
+
+        static void TestSendMailMessage()
+        {
+
+            string server = "smtp-mail.outlook.com";
+            var from = new MailAddress("ccp.adm@outlook.com");
+            //var to = new MailAddress("christian.popescu@outlook.com");
+            var to = new MailAddress("alexvpopescu@live.com");
+            var message = new MailMessage(from, to)
+            {
+                Subject = "Using the SmtpClient class.",
+                Body = @"Using this feature, you can send an e-mail message from an application very easily.\n Urs"
+            };
+            var client = new SmtpClient(server)
+            {
+                Port = 587,
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new System.Net.NetworkCredential("ccp.adm@outlook.com", "Alex1997"),
+                
+            };
+
+            try
+            {
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in CreateTestMessage4(): {0}",
+                            ex.ToString());
+            }
+        }
+    }
+}
