a33898bf155520e146679d3c63d1e31a12e84540
Christian Popescu <christian.popescu@outlook.com>
2020-09-28 22:19:37 +0200

    add branch class and status line

---
+++ src/CCPWinFramework/CommandLineTools/SourceControlTools/Git/GitStatusLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineTools.SourceControlTools.Git
+{
+
+    /// <summary>
+    ///  Represents one line in the answer of "get status --porcelain=1" request
+    ///
+    ///  For details see: https://git-scm.com/docs/git-status
+    ///  </summary>
+    public class GitStatusLine
+    {
+        public char X { get; private set; }
+        public char Y { get; private set; }
+        public string Path { get; private set; }
+
+        public string Raw { get; private set; }
+
+        private void ParseStatusLine()
+        {
+            //todo to implement
+        }
+
+        // todo : constructor
+        // todo : factory
+  
+    }
+}
