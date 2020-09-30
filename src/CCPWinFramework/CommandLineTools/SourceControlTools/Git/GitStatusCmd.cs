c0cf080250c675e749349030a8580c7b309f813a
Christian Popescu <christian.popescu@outlook.com>
2020-10-01 00:26:53 +0200

    Add command status and command type

---
+++ src/CCPWinFramework/CommandLineTools/SourceControlTools/Git/GitStatusCmd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineTools.SourceControlTools.Git
+{
+    /// <summary>
+    /// Represent git status command
+    /// As it will be used to parse by default the command use porcelain parameter
+    /// For the begining porcelain is version 1
+    /// </summary>
+    public class GitStatusCmd : GitCommand
+    {
+
+        protected GitStatusCmd()
+        {
+            CommandType = GitCommandType.Status;
+        }
+        public override string GenerateCommandLineParameters()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
