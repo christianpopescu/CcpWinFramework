781a1255a6a6b3a8e6d83e619705196e37ef4b60
Christian Popescu <christian.popescu@outlook.com>
2020-09-30 23:55:34 +0200

    Add abstract GitCommand

---
+++ src/CCPWinFramework/CommandLineTools/SourceControlTools/Git/GitCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineTools.SourceControlTools.Git
+{
+    /// <summary>
+    /// Represent a generic Git command
+    /// </summary>
+
+    public abstract class GitCommand
+    {
+        public abstract  string GenerateCommandLineParameters();
+
+    }
+}
