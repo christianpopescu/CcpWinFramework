a9d8bc14ae9d2a5477928ddbce9861a13617ed7f
Christian Popescu <christian.popescu@outlook.com>
2020-10-03 22:36:29 +0200

    Add repository for workspaces

---
+++ src/CCPWinFramework/CommandLineTools/SourceControlTools/GitWorkspaceRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+///  Repository that keeps informations about available workspaces
+/// </summary>
+
+namespace CommandLineTools.SourceControlTools
+{
+    public class GitWorkspaceRepository
+    {
+    }
+}
