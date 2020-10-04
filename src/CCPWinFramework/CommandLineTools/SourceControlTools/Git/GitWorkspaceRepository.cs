0756ca75a0ba2001dfb74c6feaf3ed0a4e29fb8b
Christian Popescu <christian.popescu@outlook.com>
2020-10-05 00:35:46 +0200

    refactor code + add documentation

--- src/CCPWinFramework/CommandLineTools/SourceControlTools/Git/GitWorkspaceRepository.cs
+++ src/CCPWinFramework/CommandLineTools/SourceControlTools/Git/GitWorkspaceRepository.cs
@@ -4,10 +4,12 @@
 ///  Repository that keeps informations about available workspaces
 /// </summary>
 
+using System.Collections.Generic;
+
 namespace CommandLineTools.SourceControlTools.Git
 {
     public abstract class GitWorkspaceRepository
     {
-
+        public abstract List<GitWorkspace> GetAll();
     }
 }
